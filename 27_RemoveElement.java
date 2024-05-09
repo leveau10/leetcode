4d67688e4b99f0a14cb428f2e6427fc1469c530c
leveau10 <leovir957@gmail.com>
2024-05-09 11:51:51 -0300

    add 3 questions

---
+++ 27_RemoveElement.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int k = 0;
+        int[] array = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                array[k] = nums[i];
+                k++;
+            }
+        }
+        for (int i = 0; i < k; i++) {
+            nums[i] = array[i];
+        }
+        return k;
+    }
+}
